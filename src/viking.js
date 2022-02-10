1ec871a29330b6ac0a7b1554311bcb1482a70bf3
Nina Nyberg <nina.b.nyberg@gmail.com>
2022-02-10 10:20:35 +0200

    Iterations 3,4 added

--- src/viking.js
+++ src/viking.js
@@ -21,7 +21,7 @@ class Viking extends Soldier {
 
   receiveDamage(damage) {
     this.health -= damage;
-    if (this.health) {
+    if (this.health > 0) {
       return `${this.name} has received ${damage} points of damage`;
     } else {
       return `${this.name} has died in act of combat`;
@@ -34,10 +34,53 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking(Viking) {
+    this.vikingArmy.push(Viking);
+  }
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon);
+  }
+  vikingAttack() {
+    let randomS =
+      this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    let randomV =
+      this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+    let saxonDamage = randomS.receiveDamage(randomV.strength);
+    if (randomS.health <= 0) this.saxonArmy.pop(randomS);
+    return saxonDamage;
+  }
+
+  saxonAttack() {
+    let randomS =
+      this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    let randomV =
+      this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+    let vikingDamage = randomV.receiveDamage(randomS.strength);
+    if (randomV.health <= 0)
+      this.vikingArmy = this.vikingArmy.filter((item) => item !== randomV);
+    // this.vikingArmy.pop(randomV);
+    return vikingDamage;
+  }
+  // showStatus()
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
